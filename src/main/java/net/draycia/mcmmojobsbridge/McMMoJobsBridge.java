682f5e6772cc00ead4d9eb29be81b7184bff443c
Draycia <lonelyyordle@gmail.com>
2019-06-05 11:54:58 -0700

    Ensure necessary API is present on enable

--- src/main/java/net/draycia/mcmmojobsbridge/McMMoJobsBridge.java
+++ src/main/java/net/draycia/mcmmojobsbridge/McMMoJobsBridge.java
@@ -1,11 +1,20 @@
 package net.draycia.mcmmojobsbridge;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class McMMoJobsBridge extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        try {
+            Class.forName("com.gamingmesh.jobs.api.JobsPrePaymentEvent");
+        } catch (ClassNotFoundException e){
+            getLogger().warning("This plugin requires a version of Jobs with JobsPrePaymentEvent! Please give Jobs an update.");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
         saveDefaultConfig();
 
         getServer().getPluginManager().registerEvents(new JobsListener(this), this);
